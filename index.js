1c564609c56aa30f9a3e6c5c495d00b692863c75
mdjamilaj <mdjamilaj1gmail.com>
2020-10-09 06:03:41 +0600

    package done

---
+++ index.js
@@ -0,0 +1,18 @@
+function shadow(options) {
+    let images = document.querySelectorAll('.shadow');
+
+    if(options.shadow_type == 'hard')
+        options.shadow_type = '0px'
+    else
+        options.shadow_type = '15px';
+
+    images.forEach(image => {
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0, 0, 0, 0.12)`;
+
+        if(options.padding){
+            image.padding = '15px';
+        }
+    })
+}
+
+module.exports.shadow = shadow;
